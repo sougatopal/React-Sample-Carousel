672914ff0b586ccc8d9bad4eaf3645d37f764d99
sougato Pal <sougatopal1@gmail.com>
2020-05-02 13:43:55 +0530

    added left and right click

--- src/Carousel.js
+++ src/Carousel.js
@@ -4,25 +4,42 @@ import "./styles.css";
 export default function Carousel(props) {
   //const [isMoving,setIsMoving] = React.useState(false);
   //const itemCount = 6;
-  //const slide = 0;
+  const [slide, setSlide] = React.useState(0);
 
   const setPrev = () => {
     console.log("prev clicked");
+    if (slide !== 0) {
+      setSlide(slide - 1);
+    } else {
+      setSlide(5);
+    }
   };
   const setNext = () => {
     console.log("next clicked");
+    if (slide !== 5) {
+      setSlide(slide + 1);
+    } else {
+      setSlide(0);
+    }
   };
-  const ImgArray = props.images;
+  const imgArray = props.images;
+  console.log("slide", slide);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <div className="carousel-conatainer">
         <div className="carousel">
-          {ImgArray.map((img, index) => {
+          {imgArray.map((img, index) => {
             return (
               <img
-                className={"carousel-photo " + (index === 0 ? "initial" : "")}
+                className={
+                  "carousel-photo " +
+                  (index === slide ? "active" : "") +
+                  (index === slide - 1 ? "prev" : "") +
+                  (index === slide + 1 ? "next" : "")
+                }
                 key={index}
                 src={img}
                 alt="mytest"
